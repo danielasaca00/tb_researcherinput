a95d5492524e3ab635cd9ca34c78f7bcebde12df
danielasaca00 <daniela.santana@uabc.edu.mx>
2025-11-24 17:13:29 -0800

    Update wsgi.py

--- tb_researcherinput/wsgi.py
+++ tb_researcherinput/wsgi.py
@@ -1,16 +1,15 @@
-"""
-WSGI config for tb_researcherinput project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
 
 import os
+import sys
 
+# Añade la ruta a tu proyecto
+project_home = '/home/tu_usuario/tb_researcherinput'
+if project_home not in sys.path:
+    sys.path.append(project_home)
+
+# Indica tu archivo settings.py
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tb_researcherinput.settings'
+
+# Carga la aplicación WSGI de Django
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tb_researcherinput.settings')
-
 application = get_wsgi_application()
